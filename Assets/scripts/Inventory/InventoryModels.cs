c45915dad64fdc730ac2d1fcd16de7b06540cd0e
Pau-max <a24pauceppor@inspedralbes.cat>
2026-02-13 17:32:51 +0100

    Inventary

--- Assets/scripts/Inventory/InventoryModels.cs
+++ Assets/scripts/Inventory/InventoryModels.cs
@@ -6,7 +6,7 @@ public class Item
     public int itemID;
     public string itemName;
     public string description;
-
+    public int rarity; // Ampliación requerida por la práctica
 }
 
 public class InventoryEntry
